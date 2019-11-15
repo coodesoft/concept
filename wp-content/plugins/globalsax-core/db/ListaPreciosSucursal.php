0d59f1a3cb3ccd20299705af8c3ea412a3077d89
Leonardo <maikndawer@gmail.com>
2019-11-15 11:49:37 -0300

    se agregó recuperación

--- wp-content/plugins/globalsax-core/db/ListaPreciosSucursal.php
+++ wp-content/plugins/globalsax-core/db/ListaPreciosSucursal.php
@@ -22,9 +22,36 @@ class ListaPreciosSucursal extends GSModel{
         }
     }
 
-    static function add($params){
+    static function validate($params){
+      if ( !$params['sucursal_id'] || !is_numeric($params['sucursal_id']) )
+        return false;
 
+      if ( !$params['list_id'] || !is_numeric($params['list_id']) )
+        return false;
+
+      return true;
     }
 
+    static function add($params){
+      global $wpdb;
+      $table_name = static::getTableName('priceListSucursal');
+
+      $data = ['sucursal_id' => $params['sucursal_id'], 'list_id' => $params['list_id'] ];
+      $result = $query->insert($table_name,  $data, ['%d', '%d']);
+      if ($result !== false)
+        return true;
+      else
+        throw new Exception('Se produjo un error al asociar una lista de precios a una sucursal', 1);
+    }
+
+    static function get($sucursal_id, $list_id){
+      $data = ['sucursal_id' => $sucursal_id, 'list_id' => $list_id];
+      if (static::validate($data)){
+        $table_name = static::getTableName('priceListSucursal');
+        $query = $wpdb->prepare("SELECT * FROM $table_name WHERE sucursal_id=%d AND list_id=%d", [$sucursal_id, $list_id]);
+        return $wpdb->get_results($query, ARRAY_A);
+      } else
+        throw new Exception("Se produjo un error al recurar una relación entre lista de precio y sucursal", 1);
+    }
 
 }
