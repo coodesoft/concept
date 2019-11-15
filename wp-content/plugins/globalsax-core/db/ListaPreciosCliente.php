17378d8e5a097446570254d5cf5cf8c8bf63a89a
Leonardo <maikndawer@gmail.com>
2019-11-15 11:49:03 -0300

    se agregó validación y guardado

--- wp-content/plugins/globalsax-core/db/ListaPreciosCliente.php
+++ wp-content/plugins/globalsax-core/db/ListaPreciosCliente.php
@@ -22,9 +22,30 @@ class ListaPreciosCliente extends GSModel{
         }
     }
 
-    static function add($params){
 
+    static function validate($params){
+      if ( !$params['client_id'] || !is_numeric($params['client_id']) )
+        return false;
+
+      if ( !$params['list_id'] || !is_numeric($params['list_id']) )
+        return false;
+
+      return true;
     }
+
+    static function add($params){
+      global $wpdb;
+      $table_name = static::getTableName('priceListClient');
+
+      $data = ['client_id' => $params['client_id'], 'list_id' => $params['list_id'] ];
+      $result = $query->insert($table_name,  $data, ['%d', '%d']);
+      if ($result !== false)
+        return true;
+      else
+        throw new Exception('Se produjo un error al asociar una lista de precios a un cliente', 1);
+    }
+
+    
 
 
 }
