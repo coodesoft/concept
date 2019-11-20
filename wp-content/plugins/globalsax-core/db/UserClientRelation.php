bcb9231f14c6d6bfcb777b695e66b08256a5147c
Leonardo <maikndawer@gmail.com>
2019-11-20 12:46:37 -0300

    se agregó método para borrado y recuperación por id de cliente

--- wp-content/plugins/globalsax-core/db/UserClientRelation.php
+++ wp-content/plugins/globalsax-core/db/UserClientRelation.php
@@ -3,7 +3,7 @@
 require_once('GSModel.php');
 
 class UserClientRelation extends GSModel{
-    
+
     static function createTable(){
 
         global $wpdb;
@@ -22,14 +22,14 @@ class UserClientRelation extends GSModel{
             dbDelta( $sql );
         }
     }
-    
+
     static function validate($user_id, $client_id){
-        if (!$user_id ||  !$client_id$ || !is_numeric($user_id) || !is_numeric($client_id))
+        if (!$user_id ||  !$client_id || !is_numeric($user_id) || !is_numeric($client_id))
             return false;
-        
+
         return true;
     }
-    
+
     static function add($user_id, $client_id){
           global $wpdb;
 
@@ -37,25 +37,52 @@ class UserClientRelation extends GSModel{
 
             $table_name = self::getTableName('user_client_relation');
 
-            $result = $wpdb->insert($table_name, array('user_id' => $WP_user_id, 'client_id'=> $GS_client_id), array('%d', '%d'));
-            
+            $result = $wpdb->insert($table_name, array('user_id' => $user_id, 'client_id'=> $client_id), array('%d', '%d'));
+
             if (!$result)
                 throw new Exception('Se produjo un error al intentar asociar un cliente a un usuario wordpress. ID del cliente: '.$client_id, 1);
-            
-            
+
+
             return ['status' => true, 'insert_id' => $wpdb->insert_id];
-        } else  
+        } else
             throw new Exception('Se produjo un error de validación de entrada de datos al asociar un cliente a un usuario wordpress. ID del cliente: '.$client_id, 1);
 
     }
-    
+
+    static function delete($id){
+      if ($id && is_numeric($id)){
+        global $wpdb;
+        $table_name = static::getTableName('user_client_relation');
+        return $wpdb->delete($table_name, array('id' => $id ));
+      } else
+        throw new Exception('Se produjo un error de validación al borrar la relación entre un cliente y un usuario. ID: '. $id, 1);
+    }
+
     static function getAll(){
       global $wpdb;
       $table_name = static::getTableName('user_client_relation');
       $query = "SELECT * FROM " . $table_name;
       return $wpdb->get_results($query, ARRAY_A);
     }
-    
+
+
+    static function getClientByUserId($id){
+      if ($id && is_numeric($id)){
+        global $wpdb;
+
+        $table_name = static::getTableName('user_client_relation');
+        $client_table = static::getTableName('clientes');
+
+        $query = $wpdb->prepare("SELECT * FROM $table_name
+                                 LEFT JOIN $client_table
+                                 ON $table_name.user_id=%d
+                                 AND $table_name.client_id=$client_table.id", [$id]);
+
+        return $wpdb->get_results($query, ARRAY_A);
+      } else
+        throw new Exception('Se produjo un error de validación al recuperar un usuario a partir de la relación con el cliente. ID: '. $id, 1);
+
+    }
 }
 
 ?>
