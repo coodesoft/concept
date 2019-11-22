79b1291226e910b4d478fa50b97b720763e29942
Leonardo <maikndawer@gmail.com>
2019-11-22 15:34:04 -0300

    se borraron métodos que no se usan

--- wp-content/plugins/globalsax-core/controllers/ClientesController.php
+++ wp-content/plugins/globalsax-core/controllers/ClientesController.php
@@ -5,37 +5,36 @@ class ClientesController {
     public function __construct(){
         add_action('wp_ajax_get_sincronizar_cliente', array($this, 'sincronizar') );
         add_action('wp_ajax_gs_load_sucursales', array($this, 'loadSucursales') );
-        add_action('wp_ajax_gs_load_pricelist_by_client', array($this, 'loadPriceLists') );
     }
 
     public function sincronizar(){
         $successOperation = true;
         $errorMessage = '';
-        
+
         //$wsNormalizedClients = [];
-        
+
         $jsonClients = Requester::get('Client');
         $clients = json_decode($jsonClients, true);
 
         $storedClients = Clientes::getAll();
         $storedPriceLists = ListaPrecios::getAll();
-        
+
         $clientCriteria = new ClientesCriteria();
         $listaCriteria = new ListaPreciosCriteria();
-        
+
         Clientes::transaction();
         foreach($clients as $client){
             $client = array_change_key_case($client, CASE_LOWER);
-            
+
             $clientCriteria->prepare($client);
             $stored = Filter::filterArrayElement($storedClients, $clientCriteria);
             if (!$stored){
-                
+
                 try{
                     $result = Clientes::add($client);
-                    
+
                     if($result['status']){
-                        
+
                         // Vinculo las listas de precios al cliente (se pasa el id interno de wordpress, no el que viene del webservice)
                         $parcial = $this->linkPriceListToClient($storedPriceLists, $client['pricelist'], $result['insert_id'], $listaCriteria);
 
@@ -47,8 +46,8 @@ class ClientesController {
                         $successOperation = false;
                         $errorMessage = 'No se pudo guardar el cliente';
                         break;
-                    }                    
-                    
+                    }
+
                 } catch (Exception $e){
                     $successOperation = false;
                     $errorMessage = $e->getMessage();
@@ -59,7 +58,7 @@ class ClientesController {
             } else{
                 // TO DO: agregar soporte para actualización y borrado
             }
-            
+
         }
 
         if ($successOperation){
@@ -73,9 +72,9 @@ class ClientesController {
         wp_die();
 
     }
-    
+
     private function linkPriceListToClient($storedLists, $priceLists, $client_id, $criteria){
-        
+
         foreach($priceLists as $listName){
 
             $criteria->prepare(['name' => $listName]);
@@ -83,16 +82,16 @@ class ClientesController {
 
             if ( $list ){
                 $data = [
-                    'client_id' => $client_id, 
+                    'client_id' => $client_id,
                     'list_id' => $list['id']
                 ];
-                ListaPreciosCliente::add($data);    
+                ListaPreciosCliente::add($data);
             }
         }
     }
-    
+
     private function linkPriceListToSucursal($listasPrecios, $storedPriceLists, $criteria, $id){
-        
+
         foreach($listasPrecios as $listaPrecioSucName){
             $criteria->prepare(['name' => $listaPrecioSucName]);
             $list = Filter::filterArrayElement($storedPriceLists, $criteria);
@@ -105,9 +104,9 @@ class ClientesController {
                 ];
                 ListaPreciosSucursal::add($data);
             }
-        }        
+        }
     }
-    
+
     private function addSucursales($sucursales, $storedPriceLists, $insert_id, $criteria){
         $result = true;
         foreach($sucursales as $sucursal){
@@ -115,40 +114,25 @@ class ClientesController {
             $sucursal['insert_id'] = $insert_id;
             $result = Sucursal::add($sucursal);
             //retorna status=true si la pudo agregar o si ya existía.
-              
+
             $listasPreciosSucursales = array_change_key_case($sucursal['pricelist'], CASE_LOWER);
             $this->linkPriceListToSucursal($listasPreciosSucursales, $storedPriceLists, $criteria, $result['insert_id'] );
         }
-    }    
-    
+    }
+
     public function loadSucursales(){
         if (isset($_POST['client'])){
-            $sucursales = Sucursal::getByClientId($_POST['client']);       
+            $sucursales = Sucursal::getByClientId($_POST['client']);
             $count = count($sucursales);
-            
+
             $return = $count ? [ 'state' => State::LIST_SUCURSALES, 'data'  => $sucursales ] :
                                [ 'state' => State::NO_SUCURSALES, 'data'  => null ];
         } else
             $return = [ 'state' => State::PARAM_ERROR, 'data'  => null ];
-        
-        echo json_encode($return);
-        wp_die();
-    }
-    
-    public function loadPriceLists(){
-        if (isset($_POST['client'])){
-            $priceLists = ListaPreciosCliente::getByClientId($_POST['client']);
-            $count = count($priceLists);
-            
-            $return = $count > 1 ? [ 'state' => State::MULTIPLE_PRICELIST, 'data'  => $priceLists ] :
-                                   [ 'state' => State::SINGLE_PRICELIST, 'data'  => $priceLists[0] ];
-        } else
-            $return = [ 'state' => State::PARAM_ERROR, 'data'  => null ];
-        
-        echo json_encode($return);
-        wp_die();
-    }
 
+        echo json_encode($return);
+        wp_die();
+    }
 }
 
 $clientesController = new ClientesController();
