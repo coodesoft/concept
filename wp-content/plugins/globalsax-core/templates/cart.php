e6eff03106af943fdfdb858bd72edc73c70165a8
Leonardo <maikndawer@gmail.com>
2019-11-28 14:04:24 -0300

    se actualizó el carrito

--- wp-content/plugins/globalsax-core/templates/cart.php
+++ wp-content/plugins/globalsax-core/templates/cart.php
@@ -2,7 +2,18 @@
 /**
  * Cart Page
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart.php.
  *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,7 +24,9 @@ require_once(__DIR__.'/../db/UserClientRelation.php');
 add_shortcode( 'gbs_cart_template', 'gbs_cart');
 function gbs_cart($atts){
 
-    if ( !is_admin() ){ ?>
+    if ( !is_admin() ){
+
+        ?>
 
         <div id="gbsCheckout">
             <form class="woocommerce-cart-form gbs-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
@@ -26,37 +39,34 @@ function gbs_cart($atts){
                     //obtengo el usuario
                     $user = wp_get_current_user(); ?>
                     <div class="order-owner trescol"> Pedido de: <?php echo $user->user_firstname ." ". $user->user_lastname ?></div>
-
-                       <?php
+                        <?php
                             $clientes = UserClientRelation::getClientByUserId($user->ID);
                             $countClientes = count($clientes);
                             $checkoutController = new CheckoutController();
 
-                            if ($countClientes > 1)
-                                $products = $checkoutController->_calculate();
-                            elseif ($countClientes == 1){
+                            if ($countClientes > 1){
+                                $products = $checkoutController->_calculate();;
+                            } elseif ($countClientes == 1){
 
                                 $cliente = $clientes[0];
                                 $sucursales = Sucursal::getByClientId($cliente['id']);
                                 $countSucursales = count($sucursales);
 
-                                if ($countSucursales > 1)
-                                    $products = $checkoutController->_calculate();
-                                elseif ($countSucursales == 1){
+                                if ($countSucursales > 1){
+                                    $products = $checkoutController->_calculate();;
+                                } elseif ($countSucursales == 1){
                                     $sucursal = $sucursales[0];
-
                                     $listas = ListaPrecios::getBySucursal($sucursal['id']);
-																		$products = $checkoutController->_calculate($listas);
+                                    $products = $checkoutController->_calculate($listas);
                                 } else{
                                     $listas = ListaPrecios::getByCliente($cliente['id']);
-																		$products = $checkoutController->_calculate($listas);
-																	}
-                            	}
+                                    $products = $checkoutController->_calculate($listas);
+                                }
+                            }
                         ?>
-                    <div class="gsSelectorContent">
-                        <div class="clienteTarget"> <?php ClienteDOM::selector($clientes); ?> </div>
+											<div class="gsSelectorContent">
+												<div class="clienteTarget"> <?php ClienteDOM::selector($clientes); ?> </div>
                         <div class="sucursalTarget"><?php isset($sucursales) ? SucursalDOM::selector($sucursales) : '';?></div>
-                        <div class="priceListTarget"><?php isset($listas) ? ListaPreciosDOM::selector($listas): '' ?></div>
 
                     </div>
 		        	</div>
