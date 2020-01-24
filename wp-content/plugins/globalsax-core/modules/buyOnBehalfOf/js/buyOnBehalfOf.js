e8ef3d58b401ad17b60485adf211aa8da363be65
Leonardo <maikndawer@gmail.com>
2020-01-24 14:15:04 -0300

    se creó la clase js encargada de manejar la ui

--- wp-content/plugins/globalsax-core/modules/buyOnBehalfOf/js/buyOnBehalfOf.js
+++ wp-content/plugins/globalsax-core/modules/buyOnBehalfOf/js/buyOnBehalfOf.js
@@ -1,7 +1,25 @@
 
 
-$(function($){
+class BuyOnBehalfOfModule{
 
 
+  constructor(){
+    this.root = document.getElementById('page');
 
-})jQuery();
+    this.bar = document.getElementById('buyOnBehalfOf');
+
+    if (typeof(this.bar) == 'undefined' || this.bar == null){
+      this.bar = document.createElement('div');
+      this.bar.id = 'buyOnBehalfOf';
+      this.root.prepend(this.bar);
+    }
+  }
+
+  check(){
+    return this.root;
+  }
+
+}
+
+const bobo = new BuyOnBehalfOfModule();
+console.log(bobo.check());
