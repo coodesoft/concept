1b5a501914ddcd8555360f3c017746c808f6cb32
Leonardo <maikndawer@gmail.com>
2020-01-24 14:14:00 -0300

    se agregó carga de estilo

--- wp-content/plugins/globalsax-core/modules/buyOnBehalfOf/BuyOnBehalfOfModule.php
+++ wp-content/plugins/globalsax-core/modules/buyOnBehalfOf/BuyOnBehalfOfModule.php
@@ -6,7 +6,7 @@ class BuyOnBehalfOfModule extends BaseModule{
     protected $modulePath = 'buyOnBehalfOf';
 
     public function __construct(){
-        parent::__construct('buyOnBehalfOf');
+        parent::__construct('buyOnBehalfOf', 'style');
     }
 }
 
